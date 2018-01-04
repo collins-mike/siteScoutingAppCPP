a5948900accfe5cea063fa63a1d84e3323453e78
Mike Collins <mike.collins@nextgenrf.com>
2018-01-04 14:23:45 -0600

    add default tests and  fxed warnings

--- SiteScoutingApp/mainwindow.cpp
+++ SiteScoutingApp/mainwindow.cpp
@@ -68,11 +68,11 @@ void MainWindow::createDefaultTests()
         testList.pop_back();
     }
 
-    testList.push_back(new TestConstainer(this, ui->plot, specan, 0, "test1", 100e6, .010, 20, 1000e6, 1000e6, -75 ));
-    testList.push_back(new TestConstainer(this, ui->plot, specan, 0, "test2", 100e6, .010, 20, 1000e6, 1000e6, -75 ));
-    testList.push_back(new TestConstainer(this, ui->plot, specan, 0, "test3", 100e6, .010, 20, 1000e6, 1000e6, -75 ));
-    testList.push_back(new TestConstainer(this, ui->plot, specan, 0, "test4", 100e6, .010, 20, 1000e6, 1000e6, -75 ));
-    testList.push_back(new TestConstainer(this, ui->plot, specan, 0, "test5", 100e6, .010, 20, 1000e6, 1000e6, -75 ));
+    testList.push_back(new TestConstainer(this, ui->plot, specan, 0, "5500 MHz", 157.1e3, .010, 20, 5500e6, 1e9, -60 ));
+    testList.push_back(new TestConstainer(this, ui->plot, specan, 1, "4000 MHz", 157.1e3, .010, 20, 4000e6, 1e9, -60 ));
+    testList.push_back(new TestConstainer(this, ui->plot, specan, 2, "915 MHz", 39.45e3, .010, 20, 915e6, 100e6, -60 ));
+    testList.push_back(new TestConstainer(this, ui->plot, specan, 3, "863 MHz", 39.45e3, .010, 20, 863e6, 100e6, -60 ));
+    testList.push_back(new TestConstainer(this, ui->plot, specan, 4, "Wide Band(30~6000 MHz)", 39.45e3, .010, 20, 3015e6, 5970e6, -60 ));
 }
 
 void MainWindow::on_btn_FindSpecan_clicked()
@@ -101,17 +101,17 @@ void MainWindow::drawPlot()
     ui->plot->replot();
 
     QString msg="";
-    for(int i=0; i<testList.size();i++)
+    for(unsigned int i=0; i<testList.size();i++)
     {
         msg+=testList[i]->getName();
     }
-    ui->runInfoLabel->setText(msg);
+    ui->runInfoLabel->setText(msg + " ");
 }
 
 
 void MainWindow::on_btn_RunTest_clicked()
 {
-    for(int i=0; i<testList.size();i++)
+    for(unsigned int i=0; i<testList.size();i++)
     {
         testList[i]->RunSweep();
     }
